c792a57d57b239340f84e4ca84c0c9ac3c3fc0b5
miguemcu <m.mejia1@udea.edu.co>
2026-09-25 11:06:43 -0500

    feat: initialize project

---
+++ src/main/java/com/udea/lab2p2026/Lab2p2026Application.java
@@ -0,0 +1,13 @@
+package com.udea.lab2p2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2p2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2p2026Application.class, args);
+	}
+
+}
